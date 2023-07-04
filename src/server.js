d58c589ca080bc18351eb8993815d9a9a7e51bc8
ridhailham <ridhailham33@gmail.com>
2023-07-04 23:07:21 +0700

    webservice-klasifikasi-ads

---
+++ src/server.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const dotenv = require('dotenv')
+
+const cors = require('cors');
+
+const app = express()
+dotenv.config()
+
+let whitelist = ['http://localhost:8080']
+let corsOptions = {
+    origin: function(origin, callback) {
+        if(whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by cors'))
+        }
+    },
+}
+
+app.use(cors(corsOptions))
+app.use(express.json())
+app.use(express.urlencoded({
+        extended: true
+    })
+)
+
+const db = require('./models/index')
+db.sequelize
+.sync({ force: true })
+.then(() => {
+    console.log('database connected');
+}).catch((err) => {
+    console.log('database connected failed');
+});
+
+app.get('/', (req, res) => {
+    res.json({
+        massage: 'server is running'
+    })
+})
+
+const PORT = 4000
+app.listen(PORT, () => {
+    console.log(`server is running on ${PORT}`)
+})
